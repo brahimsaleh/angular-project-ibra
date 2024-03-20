4cb70ac21464faf2d8540c088afc3699aa3bb283
ibra <ibrahimsm@msmk.university>
2024-03-20 13:40:06 +0100

    booleanos y operadores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,45 +13,95 @@ export class AppComponent {
 
 
 
-  name:  string = 'jorge'
-  age:  number = 32
+  name: string = 'jorge'
+  age: number = 32
   friends: Array<string> = ["2", "3"];
-  random: Array<any>= ["HOLA, true, 25"];
+  random: Array<any> = ["HOLA, true, 25"];
 
   resultado: number | string = '';
   data1: number = 0;
   data2: number = 0;
 
-  public setData1(event :any){
-
-   this.data1 = Number(event.target.value)
-    
+  ngOnInit(){
+    this.executets();
   }
-  public setData2(event :any){
+
+  public setData1(event: any) {
+
+    this.data1 = Number(event.target.value)
+
+  }
+  public setData2(event: any) {
 
     this.data1 = Number(event.target.value)
   }
 
 
 
-  public calculator(number1:number,number2:number,operation:string): number | any {
-  switch (operation) {
-        case '+':
+  public calculator(number1: number, number2: number, operation: string): number | any {
+    switch (operation) {
+      case '+':
         return this.resultado = number1 + number2;
-        case '-':
+      case '-':
         return this.resultado = number1 - number2;
-        case '*':
+      case '*':
         return this.resultado = number1 * number2;
-        case '/':
+      case '/':
         return this.resultado = number1 / number2;
 
-        default:
-          this
-   
+      default:
+        this
+
         break;
 
 
-  }
-}
+    }
 
-}
+
+    
+  }
+  
+  executets() {
+    const numero1: number = 1;
+    const numero2: number = 2;
+
+    const operadores: string[] = ["+", "-", "*", "/", "&&", "||", "===", "!=="];
+    const comparacion: boolean = (1 === 1);
+    const comparacion2: boolean = (numero1 === numero2);
+    
+    
+
+    console.log("tiene que devolver: true",comparacion);
+    console.log("tiene que devolver: false",comparacion2);
+
+    console.log("tiene que devolver:true",numero1!==numero2);
+    console.log("tiene que devolver:false",2!==numero2);
+
+
+    console.log("operador:&&:true&&true",true&&true);
+    console.log("operador:&&:true&&false",true&&false);
+    console.log("operador:&&:false&&true",false&&true);
+    console.log("operador:&&:false&&false",false&&false);
+
+    console.log("operador:||:true||true",true||true);
+    console.log("operador:||:true||false",true||false);
+    console.log("operador:||:false||true",false||true);
+    console.log("operador:||:false||false",false||false);
+
+    console.log("pregunta1:devuelve true",(numero1!== 2)|| false);
+    console.log("pregunta2:devuelve false",(numero1!== 1)|| false);
+    console.log("pregunta3devuelve true",(numero1!== 1)|| true);
+    console.log("pregunta4devuelve false",(numero1!== 2)&& false);
+
+    let condicion: boolean = true;
+
+    if (condicion){
+
+      console.log("entramos en el if");
+
+    }  else {
+      console.log ("no entremos en el if")
+    }
+    
+  }
+} 
